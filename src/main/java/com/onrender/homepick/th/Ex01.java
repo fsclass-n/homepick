c5aa56d5e0fe68eab65f440f46b43600a9e055d2
wi <whyeil@naver.com>
2026-09-04 17:50:38 +0900

    11

--- src/main/java/com/onrender/homepick/th/Ex01.java
+++ src/main/java/com/onrender/homepick/th/Ex01.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Th01 {
+public class Ex01 {
 
-    @GetMapping("/th")
-    public String th01(Model model) {
+    @GetMapping("/th/ex01")
+    public String ex01(Model model) {
         model.addAttribute("message", "안녕하세요!");
-        return "th/th01_텍스트";
+        return "th/ex01";
     }
 }
